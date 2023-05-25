ec8c670684a7a44206e0230caec0b2f2c1cd2173
jd fuller <133684440+braesheightsjd@users.noreply.github.com>
2023-05-25 10:47:06 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -75,12 +75,12 @@ soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
 
-export var adventureMusic = new soundFile("silence.wav");
+export var adventureMusic = new soundFile("adv.wav");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("silence.wav");
+export var villageMusic = new soundFile("vil.wav");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
